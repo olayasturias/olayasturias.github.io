102325e8cf5b1993640c3f42d4e32ff156e1cd72
Olaya Álvarez Tuñón <olaya_93@hotmail.es>
2026-06-11 14:36:41 +0200

    build(docker): serve unpublished posts in local preview

--- bin/entry_point.sh
+++ bin/entry_point.sh
@@ -34,7 +34,7 @@ start_jekyll() {
     manage_gemfile_lock
     ensure_bundle_deps
     mkdir -p "$DOCKER_DESTINATION"
-    bundle exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling --destination "$DOCKER_DESTINATION" --config "$CONFIG_FILE" &
+    bundle exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling --unpublished --destination "$DOCKER_DESTINATION" --config "$CONFIG_FILE" &
 }
 
 start_jekyll
